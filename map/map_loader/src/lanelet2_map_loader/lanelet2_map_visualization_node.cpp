26355b012279926b827831876a88c371861deccf
Mamoru Sobue <mamoru.sobue@tier4.jp>
2024-11-18 16:08:08 +0900

    update for bus_stop_area(map_loader)
    Signed-off-by: Mamoru Sobue <mamoru.sobue@tier4.jp>

--- map/map_loader/src/lanelet2_map_loader/lanelet2_map_visualization_node.cpp
+++ map/map_loader/src/lanelet2_map_loader/lanelet2_map_visualization_node.cpp
@@ -132,13 +132,16 @@ void Lanelet2MapVisualizationNode::onMapBin(
   std::vector<lanelet::NoParkingAreaConstPtr> no_parking_reg_elems =
     lanelet::utils::query::noParkingAreas(all_lanelets);
   lanelet::ConstLineStrings3d curbstones = lanelet::utils::query::curbstones(viz_lanelet_map);
+  std::vector<lanelet::BusStopAreaConstPtr> bus_stop_reg_elems =
+    lanelet::utils::query::busStopAreas(all_lanelets);
 
   std_msgs::msg::ColorRGBA cl_road, cl_shoulder, cl_cross, cl_partitions, cl_pedestrian_markings,
     cl_ll_borders, cl_shoulder_borders, cl_stoplines, cl_trafficlights, cl_detection_areas,
     cl_speed_bumps, cl_crosswalks, cl_parking_lots, cl_parking_spaces, cl_lanelet_id,
     cl_obstacle_polygons, cl_no_stopping_areas, cl_no_obstacle_segmentation_area,
     cl_no_obstacle_segmentation_area_for_run_out, cl_hatched_road_markings_area,
-    cl_hatched_road_markings_line, cl_no_parking_areas, cl_curbstones, cl_intersection_area;
+    cl_hatched_road_markings_line, cl_no_parking_areas, cl_curbstones, cl_intersection_area,
+    cl_bus_stop_area;
   setColor(&cl_road, 0.27, 0.27, 0.27, 0.999);
   setColor(&cl_shoulder, 0.15, 0.15, 0.15, 0.999);
   setColor(&cl_cross, 0.27, 0.3, 0.27, 0.5);
@@ -163,6 +166,7 @@ void Lanelet2MapVisualizationNode::onMapBin(
   setColor(&cl_no_parking_areas, 0.42, 0.42, 0.42, 0.5);
   setColor(&cl_curbstones, 0.1, 0.1, 0.2, 0.999);
   setColor(&cl_intersection_area, 0.16, 1.0, 0.69, 0.5);
+  setColor(&cl_bus_stop_area, 0.863, 0.863, 0.863, 0.5);
 
   visualization_msgs::msg::MarkerArray map_marker_array;
 
