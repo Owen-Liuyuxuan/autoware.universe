48c7ca3ae72c3b5be0c280b4a4ba176a75d09406
Takumi Ito <takumi.ito@tier4.jp>
2024-12-04 11:13:54 -0500

    rename integral interval
    Signed-off-by: Takumi Ito <takumi.ito@tier4.jp>

--- planning/behavior_path_planner/autoware_behavior_path_planner_common/src/utils/parking_departure/geometric_parallel_parking.cpp
+++ planning/behavior_path_planner/autoware_behavior_path_planner_common/src/utils/parking_departure/geometric_parallel_parking.cpp
@@ -742,9 +742,9 @@ std::vector<PathWithLaneId> GeometricParallelParking::planOneTrialClothoid(
       2 * M_PI);
     double C_f_1 = 0;
     double S_f_1 = 0;
-    for (double u = 0; u <= std::sqrt(beta_1 / M_PI); u += integral_interval_) {
-      C_f_1 += std::cos(M_PI_2 * std::pow(u, 2)) * integral_interval_;
-      S_f_1 += std::sin(M_PI_2 * std::pow(u, 2)) * integral_interval_;
+    for (double u = 0; u <= std::sqrt(beta_1 / M_PI); u += clothoid_integral_interval_) {
+      C_f_1 += std::cos(M_PI_2 * std::pow(u, 2)) * clothoid_integral_interval_;
+      S_f_1 += std::sin(M_PI_2 * std::pow(u, 2)) * clothoid_integral_interval_;
     }
     const double A_new_1 =
       std::sqrt(1 / M_PI) * std::abs(
@@ -770,9 +770,9 @@ std::vector<PathWithLaneId> GeometricParallelParking::planOneTrialClothoid(
       2 * M_PI);
     double C_f_2 = 0;
     double S_f_2 = 0;
-    for (double u = 0; u <= std::sqrt(beta_2 / M_PI); u += integral_interval_) {
-      C_f_2 += std::cos(M_PI_2 * std::pow(u, 2)) * integral_interval_;
-      S_f_2 += std::sin(M_PI_2 * std::pow(u, 2)) * integral_interval_;
+    for (double u = 0; u <= std::sqrt(beta_2 / M_PI); u += clothoid_integral_interval_) {
+      C_f_2 += std::cos(M_PI_2 * std::pow(u, 2)) * clothoid_integral_interval_;
+      S_f_2 += std::sin(M_PI_2 * std::pow(u, 2)) * clothoid_integral_interval_;
     }
     const double A_new_2 =
       std::sqrt(1 / M_PI) * std::abs(
@@ -1017,7 +1017,7 @@ PathWithLaneId GeometricParallelParking::generateClothoidPath(
   const bool is_left_steering, const bool is_forward)
 {
   PathWithLaneId path;
-  const int NUM_STEPS = L / integral_interval_;
+  const int NUM_STEPS = L / clothoid_integral_interval_;
 
   const auto generatePathPointFromXYYaw = [](
                                             const Pose & start_pose, const double x, const double y,
@@ -1040,10 +1040,10 @@ PathWithLaneId GeometricParallelParking::generateClothoidPath(
   path.points.push_back(generatePathPointFromXYYaw(start_pose, x, y, yaw));
 
   for (int i = 0; i <= NUM_STEPS; ++i) {
-    double curvature = i * integral_interval_ / (R * L);
-    x += integral_interval_ * std::cos(yaw);
-    y += integral_interval_ * std::sin(yaw);
-    yaw += integral_interval_ * curvature;
+    double curvature = i * clothoid_integral_interval_ / (R * L);
+    x += clothoid_integral_interval_ * std::cos(yaw);
+    y += clothoid_integral_interval_ * std::sin(yaw);
+    yaw += clothoid_integral_interval_ * curvature;
     if (std::hypot(x - x_prev, y - y_prev) > arc_path_interval) {
       x_prev = x;
       y_prev = y;
